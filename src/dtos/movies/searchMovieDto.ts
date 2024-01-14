cadcd35cb8bf24586efd125a5a762ce3e83d2da5
Hugo <hlarrea@fi.uba.ar>
2024-01-14 17:29:16 -0300

    search movie refactor

--- src/dtos/movies/searchMovieDto.ts
+++ src/dtos/movies/searchMovieDto.ts
@@ -2,8 +2,10 @@ import Joi from "joi";
 
 export class SearchMovieDto {
     query: string;
+    page: number;
 }
 
 export const SearchMovieSchema = Joi.object({
     query: Joi.string().required(),
+    page: Joi.number(),
 });
