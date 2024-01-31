c671263eb40bf0eaacc50b616b4ccb9f9644aa09
Hugo <hlarrea@fi.uba.ar>
2024-01-30 23:30:12 -0300

    Get season details

--- src/controllers/seriesController.ts
+++ src/controllers/seriesController.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { Request } from '@models';
 import { config } from '@config';
-import { SearchContentDto } from '@dtos';
+import { GetSeasonDto, GetSeriesDto, SearchContentDto } from '@dtos';
 
 class SeriesController {
 
@@ -17,8 +17,18 @@ class SeriesController {
         return response.data;
     }
 
-    public async getSeries(req: Request<SearchContentDto>) {
+    public async getSeries(req: Request<GetSeriesDto>) {
         const response = await axios.get(`${this.SERIES_URL}/${req.params.seriesId}`, {
+            params: {
+                country: req.query.country,
+            },
+            headers: { Authorization: `${req.headers.authorization}` }
+        });
+        return response.data;
+    }
+
+    public async getSeason(req: Request<GetSeasonDto>) {
+        const response = await axios.get(`${this.SERIES_URL}/${req.params.seriesId}/seasons/${req.params.seasonId}`, {
             params: {
                 country: req.query.country,
             },
--- src/dtos/series/index.ts
+++ src/dtos/series/index.ts
@@ -1,1 +1,2 @@
 export * from "./getSeriesDto"
+export * from "./getSeasonDto"
