6fdf15ee118f94aa438a57192b48684e51cd5953
dbalestieri <dbalestieri@mulesoft.com>
2024-04-08 18:30:53 -0300

    Add recommendations to series

--- src/controllers/seriesController.ts
+++ src/controllers/seriesController.ts
@@ -1,9 +1,18 @@
 import { Request } from '@models';
-import { SERIES_URL } from '@config';
+import { SERIES_REC_URL, SERIES_URL, config } from '@config';
 import { GetSeasonDto, GetSeriesDto, SearchContentDto } from '@dtos';
 import { authorizedGet } from 'utils';
 
 class SeriesController {
+
+    private async getSeriesDetails(
+        seriesId: string,
+        authorization: string,
+        query: any
+      ): Promise<any> {
+        return await authorizedGet(`${SERIES_URL}/${seriesId}`, authorization,
+          { ...query });
+      }
 
     public async searchSeries(req: Request<SearchContentDto>) {
         return await authorizedGet(`${SERIES_URL}`, req.headers.authorization, {
@@ -12,9 +21,27 @@ class SeriesController {
     }
 
     public async getSeries(req: Request<GetSeriesDto>) {
-        return await authorizedGet(`${SERIES_URL}/${req.params.seriesId}`, req.headers.authorization, {
-            ...req.query,
-        });
+        const seriesDetails = await this.getSeriesDetails(req.params.seriesId, req.headers.authorization, req.query);
+        const similar: any = []
+        try {
+            const recommendations = await authorizedGet(`${SERIES_REC_URL}/${req.params.seriesId}`, req.headers.authorization,
+            { ...req.query }, {'Secret': config.rapiSecret});
+            for (const movie of recommendations.slice(0,3)) { //TODO: sacar el slice, esto esta ahora por los problemas de performance
+                const recommendation = await this.getSeriesDetails(movie.id, req.headers.authorization, req.query);
+                similar.push({
+                    "id": recommendation.id,
+                    "title": recommendation.title,
+                    "posterPath": recommendation.poster,
+                    "releaseDate": recommendation.releaseDate
+                });
+            };
+        } catch (error) {
+            console.error("Error fetching recommendations: ", error.message)
+        }
+        if (similar.length > 0) {
+            seriesDetails.similar = similar;
+        }
+        return seriesDetails;
     }
 
     public async getCredits(req: Request<GetSeriesDto>) {
